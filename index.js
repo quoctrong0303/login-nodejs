d9d09fd99d52cb99e0105a400634dbeeb2aa415d
quoctrong0303 <quoctrong0303@gmail.com>
2022-12-13 23:27:34 +0700

    first commit

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ app.post("/quest-claim", async (request, response) => {
 });
 
 app.post("/quest-skip", async (request, response) => {
-    console.log(request.body.idToken);
     //Lấy token từ body
     let res = await api.skipWaiting(request.body.idToken);
     response.send(res.data);
